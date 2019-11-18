d60f09a6e648de3808229c713809046ed53ee8fa
Dahveyea <dahveyea.cowan@gmail.com>
2019-11-17 20:42:28 -0800

    Added questions

---
+++ quiz.py
@@ -0,0 +1,19 @@
+# Multiple Choice
+Question1 = "What is the oldest anime? \n a) One Piece  \n b) Naruto  \n c) Doraemon  \n d) Dragon Ball Z  \n e) Sazae-san\n"
+# The answer is e)
+
+Question2 = """When did One piece first air
+\n a) 1998  \n b) 1997 \n c) 1996 \n d) 1999  \n e) 1995\n"""
+# The answer is d)
+
+# Numerical Response
+Question3 = "How many pokemon are there as currently?"
+# The answer is 802
+
+Question4 = "How many members are there in the Akatsuki in Naruto"
+# The answer is 10
+
+
+# True/False
+Question5 = """Avatar is considered an anime?"""
+# True
