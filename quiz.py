000b1d6493e7ba64679079397c63768c5573f697
Dahveyea <dahveyea.cowan@gmail.com>
2019-11-20 01:01:53 -0800

    Added replay and fixed some bugs

--- quiz.py
+++ quiz.py
@@ -18,9 +18,9 @@ Question4 = "How many members are there in the Akatsuki in Naruto"
 Question5 = """Avatar is considered an anime?"""
 # True
 
-
+questions_wrong = list()
+questions_right = list()
 score = 0
-
 
 
 def multiple_choice_question(question, answer):
@@ -30,11 +30,28 @@ def multiple_choice_question(question, answer):
         print("You are correct!")
         global score
         score +=1
+        questions_right.append("question 1 was right")
         return 1
     else:
         print("Incorrect, The answer is: ", answer)
         score -=1
+        questions_wrong.append("question 1 was wrong")
         return 0
+
+
+def multiple_choice_questions(question, answer):
+    print(question)
+    user_answer = input("Input a choice (a-e): \n").lower()
+    if user_answer == answer:
+        print("You are correct!")
+        global score
+        score +=1
+        questions_right.append("question 2 was right")
+        return 1
+    else:
+        print("Incorrect, The answer is: ", answer)
+        score -=1
+        questions_wrong.append("question 2 was wrong")
 
 
 def numerical_question(question, answer):
@@ -44,13 +61,36 @@ def numerical_question(question, answer):
         print("You are correct!")
         global score
         score +=1
+        questions_right.append("question 3 was right")
         return 1
     # elif user_answer == answer and user_answer.isdigit == False:
     #     print("Inproper format")
     else:
         print("Incorrect, The answer is: ", answer)
         score -=1
+        questions_wrong.append("question 3 was wrong")
         return 0
+
+
+
+def numerical_questions(question, answer):
+    print(question)
+    user_answer = input("Input a number: \n")
+    if user_answer == answer and user_answer.isdigit():
+        print("You are correct!")
+        global score
+        score +=1
+        questions_right.append("question 4 was right")
+        return 1
+    # elif user_answer == answer and user_answer.isdigit == False:
+    #     print("Inproper format")
+    else:
+        print("Incorrect, The answer is: ", answer)
+        score -=1
+        questions_wrong.append("question 4 was wrong")
+        return 0
+
+
 
 def true_false_question(question, answer, fact):
     print(question)
@@ -59,27 +99,55 @@ def true_false_question(question, answer, fact):
         print("You are correct!")
         global score
         score +=1
+        questions_right.append("question 5 was right")
         return 1
     else:
         print("Incorrect, The answer is: ", answer, '\n', fact)
         score -=1
+        questions_right.append("question 5 was wrong")
         return 0
 
 
-multiple_choice_question(Question1, 'e')
-multiple_choice_question(Question2, 'd')
 
-numerical_question(Question3, '802')
-numerical_question(Question4, '10')
+def summary():
+    print("Your final score is " + str(score) + " out of 5")
+    if score == 4:
+        print("You are a rising weeb")
 
-true_false_question(Question5, 't', 'Its only considered an cartoon')
+    elif score == 5:
+        print("You are a full fledge weeb")
+    else:
+        print("You're not a weeb at all \n")
 
-print("Your final score is " + str(score) + " out of 5")
-if score == 3:
-    print("You are a rising weeb")
 
-elif score == 5:
-    print("You are a full fledge weeb")
-else:
-    score < 3
-    print("You're not a weeb at all")
+
+    if questions_wrong == 0:
+        print("                 ")
+        print(questions_right)
+
+
+
+def anime_quiz():
+    print("Quiz time! Let's test your knowledge of anime\n")
+    multiple_choice_question(Question1, 'e')
+    multiple_choice_questions(Question2, 'd')
+    numerical_question(Question3, '802')
+    numerical_questions(Question4, '10')
+    true_false_question(Question5, 't', 'Its only considered an cartoon')
+    summary()
+
+
+
+
+if __name__ == "__main__":
+    playing = True
+    while playing:
+        anime_quiz()
+        play_again = input("Would you like to retake this quiz? Enter y or n\n".lower())
+        if play_again == "y":
+            questions_wrong = list()
+            questions_right = list()
+            score = 0
+            anime_quiz()
+        else:
+            playing = False
